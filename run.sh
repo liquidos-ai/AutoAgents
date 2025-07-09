749604e11a18f8df045dbf01223f46f94c6c4536
Harshal Rembhotkar <harshalrembhotkar94@gmail.com>
2025-07-09 13:46:16 +0530

    Delete run.sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Load environment variables from .env file
-if [ -f .env ]; then
-  export $(cat .env | sed 's/#.*//g' | xargs)
-fi
-
-# Run the streaming agent example
-cargo run -p streaming_agent
